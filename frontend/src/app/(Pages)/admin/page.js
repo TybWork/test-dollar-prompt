00d8b9c04368d33720ccb247980ef386148365ac
Muduasir Bashir <for.all6474@gmail.com>
2024-08-07 16:54:00 +0500

    getCookieValue function in admin set

--- frontend/src/app/(Pages)/admin/page.js
+++ frontend/src/app/(Pages)/admin/page.js
@@ -19,13 +19,13 @@ const page = () => {
         }
     }
 
+    const getCookieValue = (name) => {
+        const match = document.cookie.match(new RegExp('(^| )' + name + '=([^;]+)'));
+        return match ? decodeURIComponent(match[2]) : null;
+    };
     useEffect(() => {
         if (typeof window !== 'undefined') {
             if (document.cookie) {
-                const getCookieValue = (name) => {
-                    const match = document.cookie.match(new RegExp('(^| )' + name + '=([^;]+)'));
-                    return match ? decodeURIComponent(match[2]) : null;
-                };
 
                 const token = getCookieValue('token');
                 const decodeCookie = jwtDecode(token);
