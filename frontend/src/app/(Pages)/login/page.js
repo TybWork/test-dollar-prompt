5d2f627496345ef77a7c2a78780eada2a17f9d57
Muduasir Bashir <for.all6474@gmail.com>
2024-08-07 19:15:41 +0500

    frontend cookie set

--- frontend/src/app/(Pages)/login/page.js
+++ frontend/src/app/(Pages)/login/page.js
@@ -25,7 +25,7 @@ const loginUser = () => {
         try {
             const request = await post(`${process.env.NEXT_PUBLIC_SERVER_URL}/api/user/login`, user)
             const response = request.data
-            document.cookie = `token = ${response.token}; path=/`
+            document.cookie = `token = ${response.token}; path=/; sameSite=None; domain=test-dollar-prompt.vercel.app secure=true`
             window.location.href = '/'
         } catch (error) {
             alert(error.response.data.msg)
