ecbed027c554b011447dc68e02875029f1f0e2e4
Muduasir Bashir <for.all6474@gmail.com>
2024-08-08 17:08:06 +0500

    sellerInfo page  sending data

--- backend/src/middlewares/verifyToken.middle.js
+++ backend/src/middlewares/verifyToken.middle.js
@@ -70,37 +70,6 @@ export const isAdmin = async (req, res, next) => {
 
 // for seller
 
-// export const getUserId = async (req, res, next) => {
-//     try {
-//         const token = req.cookies.token;
-//         if (!token) {
-//             return res.status(401).json({ msg: "Unauthorized: Token not found" });
-//         }
-
-//         let decoded;
-//         try {
-//             decoded = jwt.verify(token, process.env.JWT_SECRET);
-//         } catch (err) {
-//             console.error('JWT Verification Error:', err.message);
-//             return res.status(401).json({ msg: 'Unauthorized: Invalid token' });
-//         }
-
-//         const userId = decoded.userId;
-//         if (!userId) {
-//             return res.status(404).json({ msg: "invalidToken: userId not found" });
-//         }
-
-//         // Store the user data in the request for further access
-//         req.userId = userId;
-
-//         // Proceed to the next middleware or route handler
-//         next();
-//     } catch (error) {
-//         return res.status(500).json({ msg: "Internal Server Error", error: error.message });
-//     }
-// };
-
-
 export const getUserId = async (req, res, next) => {
     try {
         const authHeader = req.headers['authorization'];
