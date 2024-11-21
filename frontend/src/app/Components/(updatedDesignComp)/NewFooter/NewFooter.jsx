eed962bc8f4ff4411e7804fcee442b10956c0186
TybWork <Dollarprompts@gmail.com>
2024-11-21 19:00:52 +0500

    responsive footer has been done

--- frontend/src/app/Components/(updatedDesignComp)/NewFooter/NewFooter.jsx
+++ frontend/src/app/Components/(updatedDesignComp)/NewFooter/NewFooter.jsx
@@ -18,30 +18,32 @@ const NewFooter = () => {
                         </div>
                     </div>
 
-                    {/* promptsLinks Section */}
-                    <ul className={styles.ul}>
-                        <li className={styles.heading}>Prompts</li>
-                        <li><Link href={'/buy-prompts'}>Buy Prompts</Link></li>
-                        <li><Link href={'/sell-prompts'}>Sell Prompts</Link></li>
-                        <li><Link href={'/market'}>Trending Prompts</Link></li>
-                        <li><Link href={'/signup'}>Sign Up</Link></li>
-                    </ul>
+                    <div className={styles.linksMainSection}>
+                        {/* promptsLinks Section */}
+                        <ul className={styles.ul}>
+                            <li className={styles.heading}>Prompts</li>
+                            <li><Link href={'/buy-prompts'}>Buy Prompts</Link></li>
+                            <li><Link href={'/sell-prompts'}>Sell Prompts</Link></li>
+                            <li><Link href={'/market'}>Trending Prompts</Link></li>
+                            <li><Link href={'/signup'}>Sign Up</Link></li>
+                        </ul>
 
-                    {/* Quick Links Section */}
-                    <ul className={styles.ul}>
-                        <li className={styles.heading}>Quick Links</li>
-                        <li><Link href={'/buy-prompts#newsletter'}>Join Our Community</Link></li>
-                        <li><Link href={'/blog'}>AI News</Link></li>
-                        <li><Link href={'/market'}>Trending AI Tools</Link></li>
-                    </ul>
+                        {/* Quick Links Section */}
+                        <ul className={styles.ul}>
+                            <li className={styles.heading}>Quick Links</li>
+                            <li><Link href={'/buy-prompts#newsletter'}>Join Our Community</Link></li>
+                            <li><Link href={'/blog'}>AI News</Link></li>
+                            <li><Link href={'/market'}>Trending AI Tools</Link></li>
+                        </ul>
 
-                    {/* Contact Links Section */}
-                    <ul className={styles.ul}>
-                        <li className={styles.heading}>Contact</li>
-                        <li><a href="mailto:Contact@dollarprompt.com">Contact@dollarprompt.com</a></li>
-                        <li><a href="/">Facebook</a></li>
-                        <li><a href="/">YouTube</a></li>
-                    </ul>
+                        {/* Contact Links Section */}
+                        <ul className={styles.ul}>
+                            <li className={styles.heading}>Contact</li>
+                            <li><a href="mailto:Contact@dollarprompt.com">Contact@dollarprompt.com</a></li>
+                            <li><a href="/">Facebook</a></li>
+                            <li><a href="/">YouTube</a></li>
+                        </ul>
+                    </div>
 
                 </div>
 
@@ -50,22 +52,40 @@ const NewFooter = () => {
 
                 {/* footer texts */}
                 <ul className={styles.bottomLinks}>
-                    <li className={styles.brandName}>
+                    <li className={`${styles.desktopLinks} ${styles.brandName}`}>
                         A Project by <span>TYBWORK</span>
                     </li>
-                    <li className={styles.copyrightText}>
+
+                    <li className={`${styles.copyrightText} ${styles.mobileLinksLinks}`}>
                         © 2024 Dollar Prompts.
                     </li>
-                    <li className={styles.privacyLinks}>
+
+
+                    <li className={`${styles.privacyLinks} ${styles.desktopLinks}`}>
                         <Link href={'/tandcs'}>Term of Use</Link>
                         <Link href={'/privacy-policy'}>Privacy Policy</Link>
+                    </li>
+
+                    {/* on small screen */}
+                    <li className={styles.mobileLinks}>
+                        {/* <li className={styles.copyrightText}>
+                            © 2024 Dollar Prompts.
+                        </li> */}
+                        <li className={styles.brandName}>
+                            A Project by <span>TYBWORK</span>
+                        </li>
+
+                        <li className={styles.privacyLinks}>
+                            <Link href={'/tandcs'}>Term of Use</Link>
+                            <Link href={'/privacy-policy'}>Privacy Policy</Link>
+                        </li>
                     </li>
                 </ul>
 
             </div>
 
 
-        </footer>
+        </footer >
     )
 }
 
