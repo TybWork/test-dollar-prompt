eb81f3f1dbe0dc94db031709d716d1a5fd9836ac
TybWork <Dollarprompts@gmail.com>
2024-12-04 17:51:35 +0500

    login validation on backend side done

--- backend/src/validations/validations.js
+++ backend/src/validations/validations.js
@@ -7,3 +7,8 @@ export const signupValidation = z.object({
     email: z.string({ required_error: 'Email is required' }).email({ message: 'Invalid email address' }),
     password: z.string({ required_error: 'Password is required' }).min(6, { message: 'Minimum 6 character required' }).max(24, { message: 'Max password length should be 24' }).trim(),
 })
+
+export const loginValidation = z.object({
+    email: z.string({ required_error: 'Email is required' }).email({ message: 'Invalid email address' }),
+    password: z.string({ required_error: 'Password is required' }).min(6, { message: 'Minimum 6 character required' }).max(24, { message: 'Max password length should be 24' }).trim(),
+})
