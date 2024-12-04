0103137bd959a975b733e54e697f33457699020e
TybWork <Dollarprompts@gmail.com>
2024-12-04 18:03:27 +0500

    firstName and lastName in Validations fixed

--- backend/src/validations/validations.js
+++ backend/src/validations/validations.js
@@ -1,9 +1,9 @@
 import { z } from 'zod'
 
 export const signupValidation = z.object({
-    firstName: z.string({ required_error: "First name is required!" }),
-    lastName: z.string({ required_error: "Last Name is required!" }),
-    country: z.string({ required_error: "country is required" }),
+    firstName: z.string({ required_error: "First name is required!" }).trim().min(1, { message: 'First name is required' }),
+    lastName: z.string({ required_error: "Last Name is required!" }).trim().min(1, { message: 'First name is required' }),
+    country: z.string({ required_error: "Country is required" }).trim(),
     email: z.string({ required_error: 'Email is required' }).email({ message: 'Invalid email address' }),
     password: z.string({ required_error: 'Password is required' }).min(6, { message: 'Minimum 6 character required' }).max(24, { message: 'Max password length should be 24' }).trim(),
 })
